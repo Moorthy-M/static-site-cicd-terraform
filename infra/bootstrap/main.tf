5639ccef7ffdd625942dd79776b66ecd8af1c7fb
Moorthy-M <moorthy2652002@gmail.com>
2026-01-28 02:58:59 +0000

    Bootstrap Added for CD Role

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,138 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_s3_bucket" "static_site" {
+  bucket = "s3-moorthy-terraform-state"
+}
+
+data "aws_iam_policy_document" "trust" {
+  statement {
+    sid    = "TrustPolicyForAssumeRole"
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:Moorthy-M/static-site-cicd-terraform:ref:refs/heads/main"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "permission" {
+  statement {
+    sid = "TerraformState"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+
+    resources = [data.aws_s3_bucket.static_site.arn, "${data.aws_s3_bucket.static_site.arn}/*"]
+  }
+
+  statement {
+    sid       = "CreateStaticSiteBuckets"
+    effect    = "Allow"
+    actions   = ["s3:CreateBucket"]
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:bucketname"
+      values   = ["static-site-*"]
+    }
+  }
+
+  statement {
+    sid    = "ManageStaticSiteBuckets"
+    effect = "Allow"
+
+    actions = [
+      "s3:PutBucketPolicy",
+      "s3:PutBucketPublicAccessBlock",
+      "s3:PutBucketVersioning",
+      "s3:PutEncryptionConfiguration",
+      "s3:GetBucket*",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "arn:aws:s3:::static-site-*"
+    ]
+  }
+
+  statement {
+    sid    = "ManageStaticSiteObjects"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::static-site-*/*"
+    ]
+  }
+
+  statement {
+    sid    = "CloudFrontDeploy"
+    effect = "Allow"
+
+    actions = [
+      "cloudfront:CreateDistribution",
+      "cloudfront:UpdateDistribution",
+      "cloudfront:GetDistribution",
+      "cloudfront:CreateOriginAccessControl",
+      "cloudfront:GetOriginAccessControl",
+      "cloudfront:UpdateOriginAccessControl"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+// Create Role for CD
+resource "aws_iam_role" "cd_role" {
+  name               = "terraform-cd-static-site-role"
+  assume_role_policy = data.aws_iam_policy_document.trust.json
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+// Create Permission Policy to Create S3 and CloudFront
+resource "aws_iam_policy" "policy" {
+  name   = "terraform-static-site-cd-permission-policy"
+  policy = data.aws_iam_policy_document.permission.json
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+// Attach Permission Policy to Role
+resource "aws_iam_role_policy_attachment" "cd_role" {
+  role       = aws_iam_role.cd_role.name
+  policy_arn = aws_iam_policy.policy.arn
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ infra/bootstrap/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "s3-moorthy-terraform-state"
+    key          = "static-site-cicd-terraform/bootstrap/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
